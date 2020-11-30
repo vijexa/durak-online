48df9320675340b532016807fad19f8243e72dad
vijexa <nuzhdinvitja@gmail.com>
2020-11-30 22:20:48 +0200

    removed cache from room

--- server/src/main/scala/com/durakonline/model/Room.scala
+++ server/src/main/scala/com/durakonline/model/Room.scala
@@ -1,36 +1,13 @@
 package com.durakonline.model
 
-import com.durakonline.cache.ConditionalExpiringCache
-
 import eu.timepit.refined.auto._
-import scala.concurrent.duration._
-import cats.effect.Clock
-import cats.effect.Timer
-import cats.effect.Concurrent
-import cats.implicits._
 
-final case class Room [F[_]] private (
+final case class Room (
   name: RoomName, 
   password: RoomPassword, 
-  players: ConditionalExpiringCache[F, UUIDString, Player]
+  players: Map[UUIDString, Player]
 )
 
 final object Room {
-
-  def apply [F[_] : Clock : Timer : Concurrent](
-    name: RoomName, 
-    password: RoomPassword
-  ): F[Room[F]] = {
-    for {
-      cache <- ConditionalExpiringCache.of[F, UUIDString, Player](
-        10.minutes,
-        1.minute
-      )
-    } yield Room(
-      name,
-      password,
-      cache
-    )
-  }
 
 }
