585935568b234936adf0518557185c3d8f9dacd8
vijexa <nuzhdinvitja@gmail.com>
2020-12-01 23:52:18 +0200

    added http requests and responses

--- server/src/main/scala/com/durakonline/model/messages/Http.scala
+++ server/src/main/scala/com/durakonline/model/messages/Http.scala
@@ -2,10 +2,31 @@ package com.durakonline.model.messages
 
 import com.durakonline.model._
 
+import eu.timepit.refined.auto._
+
 import io.circe.generic.JsonCodec
 import io.circe.refined._
 
 object Http {
-  @JsonCodec case class HelloWorldMessage (test: String, hello: String)
-  @JsonCodec case class RefinedTestMessage (id: UUIDString, password: RoomPassword)
+  object Request {
+    @JsonCodec case class RefinedTest (id: UUIDString, password: RoomPassword)    
+
+    @JsonCodec case class CreatePlayer (name: UserName)
+    @JsonCodec case class CreateRoom (name: RoomName, password: RoomPassword)
+    @JsonCodec case class JoinRoom (playerId: UUIDString, roomName: RoomName, roomPassword: RoomPassword)
+  }
+
+  object Response {
+    @JsonCodec case class HelloWorld (test: String, hello: String)
+
+    @JsonCodec case class OK private  (status: ResponseStatus)
+    object OK {
+      def apply = new OK("ok")
+    }
+
+    @JsonCodec case class Error private  (status: ResponseStatus, errorDescription: String)
+    object Error {
+      def apply (errorDescription: String) = new Error("error", errorDescription)
+    }
+  }
 }
