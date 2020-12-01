15b6cdbe282c95b796925becf3a24392505c02bf
vijexa <nuzhdinvitja@gmail.com>
2020-12-01 23:56:22 +0200

    routes for player and room creation

--- server/src/main/scala/com/durakonline/LobbyRoutes.scala
+++ server/src/main/scala/com/durakonline/LobbyRoutes.scala
@@ -1,9 +1,11 @@
 package com.durakonline
 
 import com.durakonline.model.messages.Http._
+import com.durakonline.model._
 
 import cats.implicits._
 import cats.effect.Sync
+import cats.effect.concurrent.Ref
 
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
@@ -11,25 +13,75 @@ import org.http4s.circe._
 
 import io.circe.syntax._
 
-object LobbyRoutes {
+import eu.timepit.refined.auto._
 
-  def helloWorldRoutes[F[_]: Sync]: HttpRoutes[F] = {
-    val dsl = new Http4sDsl[F]{}
-    import dsl._
+import java.util.UUID
+import eu.timepit.refined.api.RefType
+
+class LobbyRoutes [F[_]: Sync] (state: Ref[F, Lobby]) {
+  val dsl = new Http4sDsl[F]{}
+  import dsl._
+
+  def helloWorldRoutes: HttpRoutes[F] = {
     HttpRoutes.of[F] {
       case GET -> Root / "hello" / name =>
-        Ok(HelloWorldMessage("testing stuff", name).asJson)
+        Ok(Response.HelloWorld("testing stuff", name).asJson)
     }
   }
 
-  def refinedTestRoutes[F[_]: Sync]: HttpRoutes[F] = {
-    val dsl = new Http4sDsl[F]{}
-    import dsl._
+  def refinedTestRoutes: HttpRoutes[F] = {
     HttpRoutes.of[F] {
       case req @ POST -> Root / "refined-test" =>
-        req.decodeJson[RefinedTestMessage].flatMap { message =>
+        req.decodeJson[Request.RefinedTest].flatMap { message =>
           implicitly[Sync[F]].delay(println(s"\n\nid: ${message.id}, pwd: ${message.password}\n\n")) *>
           Ok("nice")
+        }
+    }
+  }
+
+  def roomManagementRoutes: HttpRoutes[F] = {
+    HttpRoutes.of[F] {
+      case req @ POST -> Root / "create-room" =>
+        req.decodeJson[Request.CreateRoom].flatMap { message => 
+          
+          for {
+            newState <- state.modify(
+              lobby => {
+                val lobbyOpt = lobby.addRoom(message.name, message.password)
+                (lobbyOpt.getOrElse(lobby), lobbyOpt)
+              }
+            )
+
+            response <- newState.fold(
+              Ok(Response.Error("failed to create room").asJson)
+            )(_ =>
+              Ok(Response.OK.apply.asJson) 
+            )
+          } yield response
+        }
+    }
+  }
+
+  def playerManagementRoutes: HttpRoutes[F] = {
+    HttpRoutes.of[F] {
+
+      case req @ POST -> Root / "new-player" =>
+        req.decodeJson[Request.CreatePlayer].flatMap { message =>
+          
+          for {
+            newState <- state.updateAndGet(lobby =>
+              lobby.addPlayerToRoom(
+                Player(
+                  RefType.applyRef[UUIDString](UUID.randomUUID().toString()).toOption.get,
+                  message.name
+                ), 
+                "lobby"
+              ).getOrElse(lobby)
+            )  
+
+            resp <- Ok(s"new state is $newState")
+          } yield resp
+          
         }
     }
   }
