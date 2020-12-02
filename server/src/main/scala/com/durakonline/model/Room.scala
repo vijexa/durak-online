2b4eb069d8812bbb8e92a03f14b60cd00a250829
vijexa <nuzhdinvitja@gmail.com>
2020-12-02 21:30:29 +0200

    added removePlayer method to Room

--- server/src/main/scala/com/durakonline/model/Room.scala
+++ server/src/main/scala/com/durakonline/model/Room.scala
@@ -5,6 +5,12 @@ final case class Room (
   password: RoomPassword, 
   players: Map[UUIDString, Player]
 ) {
+  /**
+    * Returns room with added player, or an error description
+    *
+    * @param player
+    * @return
+    */
   def addPlayer (player: Player): Either[ErrorDescription, Room] = 
     players.find{ case (_, currPlayer) => currPlayer.id == player.id }
       match {
@@ -13,6 +19,15 @@ final case class Room (
         case Some(_) => 
           Left(s"player with such id already exists in room $name")
       }
+
+  /**
+    * Returns room with removed player, if any
+    *
+    * @param id
+    * @return
+    */
+  def removePlayer (id: UUIDString): Room = 
+    this.copy(players = players.filter{ case (currId, _) => currId != id })
 }
 
 final object Room {
