0c2447bfc7a91eb413241dc4fa2c31529c4d44f6
vijexa <nuzhdinvitja@gmail.com>
2020-12-04 22:34:03 +0200

    implemented deck shuffling and card dealing

--- server/src/main/scala/com/durakonline/game/Deck.scala
+++ server/src/main/scala/com/durakonline/game/Deck.scala
@@ -1,51 +1,74 @@
 package com.durakonline.game
 
+import cats.implicits._
 import cats.effect.Sync
+
+import util.Random
 
 // TODO: the idea is to be able to easily create decks of different sizes
 // and compositions, and also implement getting random card and reduce Deck
 
-final case class Deck(cards: Set[Card]) extends CardContainer {
+final case class Deck(cards: Vector[Card]) extends CardContainer {
+  private val cardsNLimit = 6
+
+  val size = cards.length
+
+  def deal (hands: Vector[Hand]): Option[(Deck, Vector[Hand])] = {
+    if (hands.size > 0) {
+      if (size > 0) {
+        hands.foldLeft((Vector.empty[Hand], cards)) {
+          case ((hands, cards), hand) => 
+            val (taken, rest) = cards.splitAt(cardsNLimit - hand.size)
+            (hands :+ hand.addCards(taken), rest)
+        } match {
+          case (hands, cards) => (this.copy(cards = cards), hands).some
+        }
+      } else None
+    } else None
+  }
+
   def addCard(card: Card): Deck = 
-    this.copy(cards = cards + card)
+    this.copy(cards = cards :+ card)
   
   protected def removeCard(card: Card): Deck = 
-    this.copy(cards = cards - card)
+    this.copy(cards = cards filterNot card.==)
+
+  protected def removeTopMostCard: Deck =
+    this.copy(cards = cards.tail)
 
   def drawCard[F[_] : Sync]: F[Option[(Card, Deck)]] = 
     implicitly[Sync[F]].delay(
       for {
-        card <- cards.iterator.drop(
-          util.Random.nextInt(cards.size)
-        ).nextOption
-      } yield (card, removeCard(card))
+        card <- cards.headOption
+      } yield (card, this.removeTopMostCard)
     )
 }
 
 object Deck  {
 
-  private def generate (lowestCard: Int) = 
-    Value.values.toSet
-      .filter(_.value >= lowestCard)
-      .flatMap(value =>
-        Suit.values.toSet.map(suit =>
-          Card(value, suit)
-        )
+  protected def generate[F[_] : Sync] (lowestCard: Int): F[Vector[Card]] = 
+    implicitly[Sync[F]].delay(
+      Random.shuffle(
+        Value.values
+          .filter(_.value >= lowestCard)
+          .flatMap(value =>
+            Suit.values.map(suit =>
+              Card(value, suit)
+            )
+          ).toVector
       )
+    )
 
   // 9, 10, J, Q, K, A
-  def of24 = Deck(
-    generate(9)
-  )
+  def of24[F[_] : Sync]: F[Deck] =
+    generate(9) map Deck.apply
 
   // 6, 7, 8, 9, 10, J, Q, K, A
-  def of36 = Deck(
-    generate(6)
-  )
+  def of36[F[_] : Sync]: F[Deck] =
+    generate(6) map Deck.apply
 
   // 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
-  def of52 = Deck(
-    generate(2)
-  )
+  def of52[F[_] : Sync]: F[Deck] = 
+    generate(2) map Deck.apply
 
 }
--- server/src/test/scala/com/durakonline/game/DeckSpec.scala
+++ server/src/test/scala/com/durakonline/game/DeckSpec.scala
@@ -17,24 +17,24 @@ class DockSpec extends AnyFlatSpec {
 
   // 9, 10, J, Q, K, A
   "Deck.of24" should "generate correct deck" in {
-    val deck = Deck.of24
+    val deck = Deck.of24[IO].unsafeRunSync()
     testDeck(deck, 9, 24)
   }
   
   // 6, 7, 8, 9, 10, J, Q, K, A
   "Deck.of36" should "generate correct deck" in {
-    val deck = Deck.of36
+    val deck = Deck.of36[IO].unsafeRunSync()
     testDeck(deck, 6, 36)
   }
 
   // 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
   "Deck.of52" should "generate correct deck" in {
-    val deck = Deck.of52
+    val deck = Deck.of52[IO].unsafeRunSync()
     testDeck(deck, 2, 52)
   }
 
   "Deck.drawCard" should "work correctly" in {
-    val deck = Deck.of36
+    val deck = Deck.of36[IO].unsafeRunSync()
 
     {
       for {
@@ -42,8 +42,36 @@ class DockSpec extends AnyFlatSpec {
       } yield for {
         (card, newDeck) <- opt
         _ = println(card)
-      } yield newDeck.cards + card shouldBe deck.cards
+      } yield card +: newDeck.cards shouldBe deck.cards
     }.unsafeRunSync
     
+  }
+
+  "Deck.deal" should "deal cards correctly" in {
+    val deck = Deck.of24[IO].unsafeRunSync()
+    // cards that aren't used in deck above
+    val spareCards = Deck.of52[IO].unsafeRunSync().cards.filter(_.value.value < 9)
+
+    val emptyHand = Hand(Set.empty)
+
+    val (dealt1, hands1) = deck.deal(Vector(emptyHand, emptyHand, emptyHand)).get
+
+    hands1.foldLeft(0)((sum, hand) => sum + hand.size) shouldBe 18
+    dealt1.size shouldBe 6
+
+    // first hand has 1 card and should get 5, second has 3 cards and will get
+    // only one because that's what left in the deck
+    val (dealt2, hands2) = dealt1.deal(
+      Vector(
+        Hand(spareCards.take(1).toSet),
+        Hand(spareCards.drop(1).take(3).toSet)
+      )
+    ).get
+
+    println(hands2)
+
+    hands2.foldLeft(0)((sum, hand) => sum + hand.size) shouldBe 10
+    dealt2.size shouldBe 0
+
   }
 }
