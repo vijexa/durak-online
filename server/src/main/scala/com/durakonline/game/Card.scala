42e78b9cdcc6d25364b2f14cd4be03c6c10b5ac2
vijexa <nuzhdinvitja@gmail.com>
2020-12-05 00:03:32 +0200

    added isTrump to Card

--- server/src/main/scala/com/durakonline/game/Card.scala
+++ server/src/main/scala/com/durakonline/game/Card.scala
@@ -1,6 +1,6 @@
 package com.durakonline.game
 
-final case class Card(value: Value, suit: Suit)
+final case class Card(value: Value, suit: Suit, isTrump: Boolean = false)
 
 object Card {
   // TODO: some parsing-verification stuff
