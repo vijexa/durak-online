d1dc1f2bdbf30d319180c33a64fb13dd0eac278e
vijexa <nuzhdinvitja@gmail.com>
2020-12-05 22:19:55 +0200

    added implicits for PlayerWithHand for convenience

--- server/src/main/scala/com/durakonline/game/PlayerWithHand.scala
+++ server/src/main/scala/com/durakonline/game/PlayerWithHand.scala
@@ -6,3 +6,8 @@ final case class PlayerWithHand(
   player: Player,
   hand: Hand
 )
+
+object PlayerWithHandImplicits {
+  implicit def implicitPlayerWithHandToHand(pwh: PlayerWithHand) = pwh.hand
+  implicit def implicitPlayerWithHandToPlayer(pwh: PlayerWithHand) = pwh.player
+}
