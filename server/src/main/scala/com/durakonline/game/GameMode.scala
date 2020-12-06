97f50a2255e14293bb62dd1c5c578dea76149c59
vijexa <nuzhdinvitja@gmail.com>
2020-12-06 20:25:52 +0200

    put GameMode  ADT into singleton

--- server/src/main/scala/com/durakonline/game/GameMode.scala
+++ server/src/main/scala/com/durakonline/game/GameMode.scala
@@ -2,6 +2,8 @@ package com.durakonline.game
 
 sealed trait GameMode 
 
-case object DeckOf24 extends GameMode
-case object DeckOf36 extends GameMode
-case object DeckOf52 extends GameMode
+object GameMode {
+  case object DeckOf24 extends GameMode
+  case object DeckOf36 extends GameMode
+  case object DeckOf52 extends GameMode
+}
