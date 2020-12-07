56a4c88a874d4a3e42a6703e5e7910121840a82d
vijexa <nuzhdinvitja@gmail.com>
2020-12-07 20:44:33 +0200

    added canAttack and canDefend to Board

--- server/src/main/scala/com/durakonline/game/Board.scala
+++ server/src/main/scala/com/durakonline/game/Board.scala
@@ -12,37 +12,43 @@ final case class Board (pairs: Vector[CardPair]) {
     }
     .collect{ case Some(value) => value }
 
+  def canAttack (card: Card): Boolean =
+    (pairsCount == 0 && pairsCount <= 6) || 
+      getAllCards.find(_.value == card.value).isDefined
+
   // should allow to add new CardPair if board is empty or there is a Card with 
   // the same value
   def attack (card: Card): Option[Board] =
-    if (
-      (pairsCount == 0 && pairsCount <= 6) || 
-      getAllCards.find(_.value == card.value).isDefined
-    ) {
+    if (canAttack(card)) {
       this.copy(pairs = pairs :+ CardPair(card, None)).some
     } else None 
+    
+  def canDefend(card: Card, target: CardPair): Boolean =
+    // should allow to defend pairs if card is with same suit and better value
+    // or it is a trump
+    if (target.defender.isEmpty)
+      if (card.isTrump && !target.attacker.isTrump) true
+      else if (card.isTrump || target.attacker.suit == card.suit)
+        if (target.attacker.value.num < card.value.num) true
+        else false
+      else false
+    else false
+
 
   def defend (card: Card, target: Card): Option[Board] = {
     val targetIndex = pairs.indexWhere(_.attacker == target)
+
     if (targetIndex >= 0) {
       val targetPair = pairs(targetIndex)
 
-      def returnSome = this.copy(
-        pairs = pairs.updated(
-          targetIndex, targetPair.copy(defender = card.some)
-        )
-      ).some
-
-      // should allow to defend pairs if card is with same suit and better value
-      // or it is a trump
-      if (targetPair.defender.isEmpty)
-        if (card.isTrump && !targetPair.attacker.isTrump) returnSome
-        else if (card.isTrump || targetPair.attacker.suit == card.suit)
-          if (targetPair.attacker.value.num < card.value.num) returnSome
-          else None
-        else None
-      else None
-
+      if (canDefend(card, targetPair)) {
+        this.copy(
+          pairs = pairs.updated(
+            targetIndex, 
+            targetPair.copy(defender = card.some)
+          )
+        ).some
+      } else None
     } else None
   }
 
