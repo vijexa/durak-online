227ad8dbccc5f2302624c19debe8a2e977eeb0ac
vijexa <nuzhdinvitja@gmail.com>
2020-12-16 20:55:55 +0200

    moved container for overlay content to its own component

--- client/src/components/lobby/LoginOverlay.tsx
+++ client/src/components/lobby/LoginOverlay.tsx
@@ -6,25 +6,7 @@ import { useCookies } from 'react-cookie'
 import fetchJsonPost from '../../util/fetchJsonPost'
 import { Status, StatusCodec } from '../../model/Status'
 import { PlayerData } from '../../model/PlayerData'
-
-const Container = styled.div`
-  font-size: 1.5em;
-  text-align: center;
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  border-radius: 0.5em;
-
-  padding: 0 1em 1em 1em;
-  background-color: white;
-
-  > * {
-    margin-top: 1em;
-  }
-`
+import PopupContainer from './PopupContainer'
 
 interface LoginOverlayProps {
 
@@ -37,7 +19,7 @@ export default function LoginOverlay (props: LoginOverlayProps) {
 
   const [name, setName] = useState('')
 
-  const newPlayer = (name: string) =>
+  const createNewPlayer = (name: string) =>
     fetchJsonPost<Status, PlayerData>('new-player', { name: name }, StatusCodec)
       .then(result => {
         console.log("resss", result)
@@ -53,11 +35,15 @@ export default function LoginOverlay (props: LoginOverlayProps) {
       closeOnDocumentClick={false}
       modal
     >
-      <Container>
+      <PopupContainer>
         <span>Enter your username to start:</span>
-        <input type="text" onChange={e => setName(e.target.value)}/>
-        <button onClick={() => {newPlayer(name)}}>Login</button>
-      </Container>
+        <input 
+          type="text" 
+          onChange={e => setName(e.target.value)} 
+          placeholder="Username"
+        />
+        <button onClick={() => {createNewPlayer(name)}}>Login</button>
+      </PopupContainer>
     </Popup>
   )
 }
---
+++ client/src/components/lobby/PopupContainer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  border-radius: 0.5em;
+
+  padding: 0 1em 1em 1em;
+  background-color: white;
+
+  > * {
+    margin-top: 1em;
+  }
+`
